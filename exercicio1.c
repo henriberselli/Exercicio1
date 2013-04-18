2903a7c5d7c1c4f6577a3b11fa898bc846e44e1a
henriberselli <henriberselli@yahoo.com.br>
2013-04-17 22:15:37 -0300

    Programa com a funcao de entrada dos dados

--- exercicio1.c
+++ exercicio1.c
@@ -1,4 +1,39 @@
-sdfhjvsjhdfbv
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define LOOP  5
+
+int in_dados (int couter);
 
 
+int main (int argc, char **argv)
+{
+	int i;
+	int vetor[LOOP];
 
+	for (i = 0; i < LOOP; i++) vetor[i] = in_dados (i);
+	printf ("Valores digitados:\n");
+	for (i = 0; i<LOOP; i++) printf ("%d - %d\n", i+1, vetor[i]);
+}
+
+int in_dados (int couter)
+{
+	int i, ok = 0;
+	char text[3];
+
+	do
+	{
+		printf ("Entre com o %d elemento do vetor\n", couter+1);
+		gets (text);
+		for (i=0; i<strlen(text); i++)
+		{
+			if (isdigit (text[i]) !=0) ok = 1;
+			else ok = 0;
+		}
+		if (ok ==0) printf ("Nao eh um numero valido!!!\n");
+		else system ("clear");
+	} while (ok == 0);
+	return (atoi(text));
+}
