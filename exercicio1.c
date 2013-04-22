7e39f6be460643bd949a1f08aaf9715ac9f273dd
henriberselli <henriberselli@yahoo.com.br>
2013-04-21 22:00:53 -0300

    Programa com a insercao dos comentarios

--- exercicio1.c
+++ exercicio1.c
@@ -6,17 +6,28 @@
 
 #define LOOP  5
 
+/*!
+\file exercicio1.c
+\brief Programa recerente ao exercicio 1 - Sistemas Embarcados
+\author Henrique
+\date Abril - 2013
+*/
+
+//Prototipos
 int in_dados (int couter);
 void ordenacao (int *valores);
 void media_desvio (int *valores);
 
+/*!
+\brief Funcao principal.
+*/
 int main (int argc, char **argv)
 {
 	int i;
 	int vetor[LOOP];
 	
-	system ("clear");
-	for (i = 0; i < LOOP; i++) vetor[i] = in_dados (i);
+	system ("clear");  //Limpa a tela
+	for (i = 0; i < LOOP; i++) vetor[i] = in_dados (i);  //Carrega vetor
 	printf ("Valores digitados:\n");
 	for (i = 0; i<LOOP; i++) printf ("%d - %d\n", i+1, vetor[i]);
 
@@ -30,16 +41,21 @@ int main (int argc, char **argv)
 	return (0);
 }
 
+/*!
+\brief Funcao de entrada de dados.
+\param couter numero do elemento a ser inserido.
+\return Retorna o valor inserido.
+*/
 int in_dados (int couter)
 {
 	int i, ok = 0;
-	char text[3];
+	char text[5];
 
-	do
+	do  //Laco para insercao do numero inteiro
 	{
 		printf ("Entre com o %d elemento do vetor\n", couter+1);
 		gets (text);
-		for (i=0; i<strlen(text); i++)
+		for (i=0; i<strlen(text); i++) //Testa se todos digitos sao numeros
 		{
 			if (isdigit (text[i]) !=0) ok = 1;
 			else 
@@ -53,27 +69,36 @@ int in_dados (int couter)
 	} while (ok == 0);
 	return (atoi(text));
 }
+
+/*!
+\brief Funcao para ordenar e mostrar os valores inseridos.
+\param *valores Endereco do vetor onde estao armazenados os numeros inseridos
+*/
 void ordenacao (int *valores)
 {
 	int vetor_aux[LOOP];
 	int i, j, k;
 
-	for (i = 0; i < LOOP; i++)
+	for (i = 0; i < LOOP; i++) //Lista os numeros inseridos
 	{
-		for (j = 0; j<= i; j++)
-		{
+		for (j = 0; j<= i; j++) //Testa se o numero eh menor que os
+		{			//numeros ja listados
 			if (j == i) vetor_aux[j] = *(valores+i);
 			if (*(valores+i) < vetor_aux[j])
-			{
+			{	//Reorganiza os numeros inseridos
 				for (k = i; j < k; k--) vetor_aux[k] = vetor_aux[k-1];
 				vetor_aux[j] = *(valores+i);
 				break;
 			}
 		}
 	}
-	for (i = 0; i < LOOP; i++) *(valores+i) = vetor_aux[i];
+	for (i = 0; i < LOOP; i++) *(valores+i) = vetor_aux[i]; //Reescreve os numeros digitados
 }
 
+/*!
+\brief Funcao para calculo da Media e Desvio Padrao
+\param *valores Endereco do vetro onde estao armazenados os numeros inseridos
+*/
 void media_desvio (int *valores)
 {
 	int i;
